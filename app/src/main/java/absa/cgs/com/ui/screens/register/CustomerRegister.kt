d70d5422de1893aa7b6ea0a1e98f009d4803c2f7
mariyarajendran <mariyarajendran55@gmail.com>
2019-12-19 08:42:10 +0530

    design changes add box details

--- app/src/main/java/absa/cgs/com/ui/screens/register/CustomerRegister.kt
+++ app/src/main/java/absa/cgs/com/ui/screens/register/CustomerRegister.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_customer_register.*
 class CustomerRegister : BaseActivity() {
 
     var moreDetailsBool: Boolean = true
+    var boxDetailsBool: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,25 +24,57 @@ class CustomerRegister : BaseActivity() {
 
     private fun init() {
         cardview_more_details_badge?.setOnClickListener {
-            moreDetailsVisibility()
+            moreDetailsVisibility(1)
+        }
+
+        cardview_box_details_badge.setOnClickListener {
+            moreDetailsVisibility(2)
         }
     }
 
 
-    private fun moreDetailsVisibility() {
-        if (moreDetailsBool) {
-            cardview_more_details.visibility = View.VISIBLE
-            updateMoreDetailBool(false, resources.getString(R.string.hide_more_details))
-        } else {
-            cardview_more_details.visibility = View.GONE
-            updateMoreDetailBool(true, resources.getString(R.string.show_more_details))
+    private fun moreDetailsVisibility(event: Int) {
+        when (event) {
+            1 -> {
+                //event one (1) Hide and show more details
+                if (moreDetailsBool) {
+                    cardview_more_details.visibility = View.VISIBLE
+                    updateMoreDetailBool(false, resources.getString(R.string.hide_more_details), event)
+                } else {
+                    cardview_more_details.visibility = View.GONE
+                    updateMoreDetailBool(true, resources.getString(R.string.show_more_details), event)
+                }
+            }
+
+            2 -> {
+                //event one (1) Hide and show box details
+                if (boxDetailsBool) {
+                    cardview_box_details.visibility = View.VISIBLE
+                    updateMoreDetailBool(false, resources.getString(R.string.hide_Box_Details), event)
+                } else {
+                    cardview_box_details.visibility = View.GONE
+                    updateMoreDetailBool(true, resources.getString(R.string.Show_Box_Details), event)
+                }
+            }
         }
     }
 
 
-    private fun updateMoreDetailBool(boolean: Boolean, textTitle: String) {
-        moreDetailsBool = boolean
-        textview_show_more_detail_badge.setText(textTitle)
+    private fun updateMoreDetailBool(boolean: Boolean, textTitle: String, event: Int) {
+        when (event) {
+            //event one (1) more details
+            1 -> {
+                moreDetailsBool = boolean
+                textview_show_more_detail_badge.setText(textTitle)
+            }
+            //event one (1) box details
+            2 -> {
+                boxDetailsBool = boolean
+                textview_show_box_detail_badge.setText(textTitle)
+            }
+        }
+
+
     }
 
     private fun setActionBarTitle() {
